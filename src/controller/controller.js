10479441d042ec4dc79491a003abf8213b130560
Quintus <qthibgit@gmail.com>
2021-02-02 01:03:16 -0600

    Added validation check for existing user

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,35 +3,39 @@ const User = db.users;
 const bcrypt = require('bcrypt')
 
 exports.register = async (req, res) => {
-    if (!req.body.name) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-        return;
-    }
+    const emailCheck = await db.users.findOne({
+        where: {email: req.body.email}
+    })
 
-    try {
-        const hashpassword = await bcrypt.hash(req.body.password, 10)
-
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashpassword
-        }).then(user => {
-            console.log("User added.")
-            res.send(user)
-        })
-    } catch (err) {
-        console.log(err)
+    if (emailCheck === null) {
+        try {
+            const hashpassword = await bcrypt.hash(req.body.password, 10)
+    
+            User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashpassword
+            }).then(user => {
+                res.send(user)
+            })
+        } catch (err) {
+            console.log(err)
+            res.status(500).send()
+        }    
+    } else {
         res.status(500).send()
     }
 
-
-    
-
     
 }
 
-exports.test = (req, res) => {
-    res.status(201).json({"test": "did it work"})
-}
+exports.getall = async (req, res) => {
+
+
+    db.users.findAll({
+        attributes: ['email']
+    })
+        .then(users => {
+            res.json(users);
+        });
+};
--- src/controller/route.js
+++ src/controller/route.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     const users = require('../controller/controller');
 
-    app.get('/test', users.test)
-    app.post('/register', users.register)
+    app.post('/register', users.register);
+    app.get('/getall', users.getall);
 }

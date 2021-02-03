fefe70707c2a8917b169e279dc4fa06578c30cdf
Quintus <qthibgit@gmail.com>
2021-02-02 23:23:41 -0600

    Added validation for password

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,7 +3,7 @@ const User = db.users;
 const bcrypt = require('bcrypt')
 
 exports.register = async (req, res) => {
-    const emailCheck = await db.users.findOne({
+    const emailCheck = await User.findOne({
         where: {email: req.body.email}
     })
 
@@ -25,17 +25,27 @@ exports.register = async (req, res) => {
     } else {
         res.status(500).send()
     }
+}
 
-    
+exports.login= async (req, res) => {
+    let getUser = await User.findOne({
+        where: {email: req.body.email}
+    })
+
+    if (!getUser) return res.status(400).send('User is not found.')
+
+    const isValid = await bcrypt.compare(req.body.password, getUser.dataValues.password)
+    if (!isValid) return res.status(400).send('The password is incorrect.')
+
+    res.send(getUser.dataValues)
 }
 
 exports.getall = async (req, res) => {
-
-
-    db.users.findAll({
-        attributes: ['email']
-    })
-        .then(users => {
-            res.json(users);
-        });
-};
+    const errb = await User.findAll();
+    try {
+        console.log("All users: ", JSON.stringify(err, null, 2));
+        res.status(500).send()
+    } catch{
+        res.status(500).send()
+    }
+}
--- src/controller/route.js
+++ src/controller/route.js
@@ -1,6 +1,9 @@
+const user = require('../db/models/user');
+
 module.exports = function(app) {
     const users = require('../controller/controller');
 
     app.post('/register', users.register);
+    app.post('/login', users.login);
     app.get('/getall', users.getall);
 }
